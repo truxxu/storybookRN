556892c448d28b92f8f65e5ebff0886a8845fad0
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-01-18 17:33:40 -0500

    created custom theme

---
+++ storybook/CustomTheme.js
@@ -0,0 +1,8 @@
+import {create} from '@storybook/theming';
+import logo from '../assets/letter.png';
+
+export default create({
+  base: 'light',
+  brandTitle: 'StorybookRN Demo',
+  brandImage: logo,
+});
--- storybook/addons.js
+++ storybook/addons.js
@@ -1,3 +1,9 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
+import {addons} from '@storybook/addons';
+import CustomTheme from './CustomTheme';
+
+addons.setConfig({
+  theme: CustomTheme,
+});
