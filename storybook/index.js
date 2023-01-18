d3881e48dba72d253131c929a6402a40a2368ddc
Daniel Trujillo <danielfetrujillo@gmail.com>
2023-01-17 21:51:33 -0500

    added sb config

--- storybook/index.js
+++ storybook/index.js
@@ -1,8 +1,8 @@
 // if you use expo remove this line
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 
-import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
-import { withKnobs } from '@storybook/addon-knobs';
+import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
+import {withKnobs} from '@storybook/addon-knobs';
 
 import './rn-addons';
 
@@ -16,10 +16,13 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: null,
+  onDeviceUI: false,
+});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+AppRegistry.registerComponent('sbdemo', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
