7a23f13f6e9a9356aca8ee3fe250f630c20a529b
Citizen Four <danielfetrujillo@gmail.com>
2023-01-18 12:09:53 -0500

    added sb loader

--- storybook/index.js
+++ storybook/index.js
@@ -4,6 +4,7 @@ import {AppRegistry} from 'react-native';
 import {getStorybookUI, configure, addDecorator} from '@storybook/react-native';
 import {withKnobs} from '@storybook/addon-knobs';
 
+import {loadStories} from './storyLoader';
 import './rn-addons';
 
 // enables knobs for all stories
@@ -11,7 +12,7 @@ addDecorator(withKnobs);
 
 // import stories
 configure(() => {
-  require('./stories');
+  loadStories();
 }, module);
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
