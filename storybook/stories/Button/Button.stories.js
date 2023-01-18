10f315892591775dbb40b192671b56aa88f00c14
Citizen Four <danielfetrujillo@gmail.com>
2023-01-18 12:30:20 -0500

    created story for custom button

--- storybook/stories/Button/Button.stories.js
+++ storybook/stories/Button/Button.stories.js
@@ -1,20 +1,26 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import { Text } from 'react-native';
-import Button from '.';
+import {action} from '@storybook/addon-actions';
+import {text} from '@storybook/addon-knobs';
+import {storiesOf} from '@storybook/react-native';
 import CenterView from '../CenterView';
+import {Button as BTest} from '../../../src/atoms';
 
 storiesOf('Button', module)
-  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () => (
-    <Button onPress={action('clicked-text')}>
-      <Text>{text('Button text', 'Hello Button')}</Text>
-    </Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  // .add('with text', () => (
+  //   <Button onPress={action('clicked-text')}>
+  //     <Text>{text('Button text', 'Hello Button')}</Text>
+  //   </Button>
+  // ))
+  // .add('with some emoji', () => (
+  //   <Button onPress={action('clicked-emoji')}>
+  //     <Text>😀 😎 👍 💯</Text>
+  //   </Button>
+  // ))
+  .add('custom', () => (
+    <BTest
+      label={text('Button text', 'Hello Button')}
+      action={action('pressed custom')}
+      disabled={false}
+    />
   ));
